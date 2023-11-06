d7888956835c8f887ff237b43017bc7be3735a8d
IWsz1 <soraa24926@gmail.com>
2023-11-06 16:13:56 +0900

    first commit　画像配置とcssの読み込み設定は完了済み

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+// 下記記述でstylesheetsフォルダ内の全てのcssファイルを読み込み
+//= link application.css
