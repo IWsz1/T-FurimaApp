6ecb45518fc1a24984e93e4fece24973de098d37
IWsz1 <soraa24926@gmail.com>
2023-11-10 22:16:28 +0900

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  # deviseへ新たに追加するメソッドをconfigure_permitted_parametersというメソッドで作ることが慣習
+  def configure_permitted_parameters
+    # deviseの受け取れるパラメーターの宣言に新しいパラメーターを追加するメソッド
+    # 第一引数にサインインサインアップアカウント情報の更新のどれかを記載　第三引数に追加したいカラム名のキーを記述する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday])
+  end
+
 end
