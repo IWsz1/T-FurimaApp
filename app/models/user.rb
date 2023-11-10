15942ac5213a05084df5857ef4e80dee46a70690
IWsz1 <soraa24926@gmail.com>
2023-11-11 03:49:46 +0900

    ログイン時のトップページ表示切替設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,28 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  # validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i} 
+  validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i} 
 
-  # with_options presence:true do
-  #   validates :nickname
-  #   # 全角ひらがな、全角カタカナ、漢字
-  #   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  #   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  #   # 全角カタカナ
-  #   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  #   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  #   validates :birthday
-  # end
-
-  with_options presence: true do
-    # 存在すること・確認用を含めて2回入力・6字以上はdeviseのデフォルト実装のため省略
-    # 半角英数字（空文字NG）以外の場合には、メッセージを出す
-    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-
-    validates :nickname, uniqueness: true
-    # @含むこと・存在することはdeviseのデフォルト実装のため省略
-    validates :email,    uniqueness: true
+  with_options presence:true do
+    validates :nickname
     # 全角ひらがな、全角カタカナ、漢字
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
@@ -35,4 +17,5 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
+
 end
