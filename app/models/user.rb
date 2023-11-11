cb7fcb766d42fdcca6c98b6e6bb80a3b6f99ee35
IWsz1 <soraa24926@gmail.com>
2023-11-11 16:51:51 +0900

    正規表現によるバリデーション追加&date_selectにメモ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 0~?番目に英字が来るかつ0~?番目に数字が来る。すべての文字が英数字で作られている。大文字小文字は問わない。
+  # \Aで、?=.で?~、.*?で~?、[a-z]でa~zが含まれることを決める、\dで数字が含まれることを決める(0~?番目に英字が来るかつ0~?番目に数字が来る。)
+  # [a-z\d]で数字又は英字、+で前述の文字列(0文字目から)で、\z最後まで終わることを決める(すべての文字が英数字で作られている。)
+  # iで大文字小文字を問わない
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Password is invalid. Include both letters and numbers'} 
 
-  validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i} 
-
-  with_options presence:true do
+  with_options presence: true do
     validates :nickname
-    # 全角ひらがな、全角カタカナ、漢字
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # 全角ひらがな、全角カタカナ、漢字、長音記号
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name is invalid. Input full-width characters' }
     # 全角カタカナ
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Last name kana is invalid. Input full-width katakana characters'}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'First name kana is invalid. Input full-width katakana characters'}
     validates :birthday
   end
-
 end
