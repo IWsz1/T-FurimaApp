320364d852bdea4e768d7f5fed7e141ba76df5c0
IWsz1 <soraa24926@gmail.com>
2023-11-12 15:26:13 +0900

    TOPページへの商品掲載&編集削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,15 @@
 class ItemsController < ApplicationController
-   # index以外のアクションを行おうとした際にログインされてなかったらログインページに遷移する設定
-   before_action :authenticate_user!, except: :index
+  # index以外のアクションを行おうとした際にログインされてなかったらログインページに遷移する設定
+  before_action :authenticate_user!, except: :index
 
   def index
+    @item = Item.includes(:user).order("created_at DESC")
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -15,6 +18,31 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(params[:id])
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:explanation,:category_id,:quality_id,:shipping_cost_id,:area_id,:shipping_date_id,:price).merge(user_id: current_user.id)
