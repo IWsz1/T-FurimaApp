69716488f8e55fc3188d497688cd8d5ea997cfd0
IWsz1 <soraa24926@gmail.com>
2023-11-12 15:37:19 +0900

    order コントローラーとorder&addressモデルの作成

---
+++ db/migrate/20231112063214_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.references :order
+      t.string :postalcode
+      t.integer :prefecture_id
+      t.string :city
+      t.string :block
+      t.string :building
+      t.timestamps
+    end
+  end
+end
